29c82addd897da9d8bf089deea47242156cac191
“Your <aleksandrovich.mikita@gmail.com>
2023-12-19 21:55:59 +0100

    Clean up

--- pages/privacy.js
+++ pages/privacy.js
@@ -3,12 +3,12 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Privacy() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      Get Post AI APP
+      Privacy Page
     </main>
   );
 }
