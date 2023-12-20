f538702eaa07e18cdad7d6e5ec94a07a0ad592cc
“Your <aleksandrovich.mikita@gmail.com>
2023-12-20 13:43:25 +0100

    Update fav icon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+        <link rel="icon" type="image/ico" sizes="32x32" href="/favicon.ico" />
+        <link rel="icon" type="image/ico" sizes="16x16" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
